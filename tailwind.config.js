844fcac6d14ce48bb9d8db4cf07221877b76d53d
sabbaghpierre <sabbaghpierre@proton.me>
2023-01-08 20:58:30 +0200

    add more content and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["index.html","/usr/lib/node_modules/daisyui"],
   theme: {
+    colors:{
+      'background':'#1E2022',
+      'card':'#393E46',
+      'button-bg':'#222831'
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
